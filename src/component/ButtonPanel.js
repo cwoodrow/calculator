d2fdde8e23c1ded53444c3e31d95f8a03dcfa398
Chris Woodrow <woodrow.chris@gmail.com>
2020-12-02 11:02:05 +0100

    Update ButtonPanel.js

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -24,7 +24,7 @@ export default class ButtonPanel extends React.Component {
         </div>
         <div>
           <Button name="7" clickHandler={this.handleClick} />
-          <Button name="8" clickHandler={this.handleClick} />
+          <Button name="7" clickHandler={this.handleClick} />
           <Button name="9" clickHandler={this.handleClick} />
           <Button name="x" clickHandler={this.handleClick} orange />
         </div>
